7ad7548a3a77c9582125041137740d616d0db2b1
Eduardogl444 <eduardogl444@gmail.com>
2024-11-30 12:41:04 -0500

    pushing code to repo

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/, 'Invalid email address'],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  {
+    toJSON: { virtuals: true },
+    id: false,
+  }
+);
+
+// Virtual: friendCount
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+module.exports = User;
